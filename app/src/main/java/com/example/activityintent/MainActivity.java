2127e0791c92e3a30c5805e69d42bdc0185bcda9
yeskikeren19 <39155099+yeskikeren19@users.noreply.github.com>
2019-02-15 11:21:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/activityintent/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.activityintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickEvent(View view){
+        Intent i = new Intent(".OtherActivity");
+        startActivity(i);
+    }
+}
